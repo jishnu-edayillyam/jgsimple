165ddeb10c023e2703e268fc9efb3f609ef88261
jishnu-edayillyam <jishnuedayillyam@gmail.com>
2021-09-24 14:42:49 +0530

    project details page main image ui update

--- src/modules/ProjectDetails/ProjectDetails.jsx
+++ src/modules/ProjectDetails/ProjectDetails.jsx
@@ -70,25 +70,12 @@ const ProjectDetails = () => {
   return (
     <div className="project-details-page">
       <section className="project-info">
-        <img
-          src="house2.jpg"
-          alt="group_image_1"
-          className="group_image group_image_1"
-        />
-        <img
-          src="house3.jpg"
-          alt="group_image_2"
-          className="group_image group_image_2"
-        />
-        <img
-          src="house4.jpg"
-          alt="group_image_3"
-          className="group_image group_image_3"
-        />
-        {/* <img src="house1.jpg" alt="main_image" className="main-image" /> */}
-        <div className="basic-info">
-          <h1>THIS IS A VERY LONG PROJECT NAME WRITTEN</h1>
-          <p>client: Some name</p>
+        <img src="house1.jpg" alt="main_image" className="main-image" />
+        <div className="basic-info-container">
+          <div className="basic-info">
+            <h1>THIS IS A VERY LONG PROJECT NAME WRITTEN</h1>
+            <p>client: Some name</p>
+          </div>
         </div>
       </section>
       <section className="about-the-work">
