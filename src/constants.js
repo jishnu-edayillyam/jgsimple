68ed3ea1777bf4e7e9a29cd75ae190503d7cb1c5
jishnu-edayillyam <jishnuedayillyam@gmail.com>
2021-09-26 21:24:22 +0530

    contact page design

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,11 @@
+/* eslint-disable prefer-destructuring */
+// export const { EMAILJS_USER_ID, EMAILJS_TEMPLATE_ID, EMAILJS_SERVICE_ID } =
+//   process.env;
+
+export const EMAILJS_USER_ID = "user_O4XGwgs7il1ptVIDWrXTf";
+export const EMAILJS_TEMPLATE_ID = "template_z3xxise";
+export const EMAILJS_SERVICE_ID = "service_08yp7ch";
+
 export const IMAGEFILE = "image_file";
 export const VIDEOFILE = "video_file";
 
--- src/modules/Contact/Contact.jsx
+++ src/modules/Contact/Contact.jsx
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import Email from "../../components/Email/Email";
 import "./style.scss";
 
 const Contact = () => {
+  const [displayEmailModal, setDisplayEmailModal] = useState(false);
+
   return (
-    <div className="contact-page">
-      <section className="text-contents">
-        <h1>CONTACT US</h1>
-        <div className="social-media">asd</div>
-      </section>
-      <iframe
-        title="JG Architects office"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d973.9253029131024!2d75.0680768291936!3d12.469607999449169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDI4JzEwLjYiTiA3NcKwMDQnMDcuMSJF!5e0!3m2!1sen!2sin!4v1632578708554!5m2!1sen!2sin"
-        allowFullScreen=""
-        loading="lazy"
-      />
-      <footer>Developed by Jishnu Edayillyam</footer>
-    </div>
+    <>
+      {displayEmailModal && (
+        <Email closeEmailModal={() => setDisplayEmailModal(false)} />
+      )}
+      <div className="contact-page">
+        <div className="text-contents">
+          <h1>CONTACT US</h1>
+          <section className="email-social-media-container">
+            <button
+              type="button"
+              className="email-button"
+              onClick={() => setDisplayEmailModal(true)}
+            >
+              <img src="email_logo.svg" alt="email-logo" />
+              EMAIL US
+            </button>
+            <div className="social-media">
+              <button type="button">
+                <img
+                  src="instagram_logo.svg"
+                  alt="instagram_logo"
+                  className="social-media-icon"
+                />
+              </button>
+              <button type="button">
+                <img
+                  src="facebook_logo.svg"
+                  alt="facebook_logo"
+                  className="social-media-icon"
+                />
+              </button>
+            </div>
+          </section>
+        </div>
+        <iframe
+          title="JG Architects office"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d973.9253029131024!2d75.0680768291936!3d12.469607999449169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDI4JzEwLjYiTiA3NcKwMDQnMDcuMSJF!5e0!3m2!1sen!2sin!4v1632578708554!5m2!1sen!2sin"
+          allowFullScreen=""
+          loading="lazy"
+        />
+        <footer>Developed by Jishnu Edayillyam</footer>
+      </div>
+    </>
   );
 };
 
