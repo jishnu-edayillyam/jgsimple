d6bba063135fef14a9b0644fec91e6fc0c73f7f1
jishnu-edayillyam <jishnuedayillyam@gmail.com>
2022-02-23 22:09:51 +0530

    removed 3d

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -11,7 +11,6 @@ const mediaFiles = [
   "https://hips.hearstapps.com/pop.h-cdn.co/assets/17/24/640x320/landscape-1497533116-not-dead.gif",
   "https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg",
   "https://i.pinimg.com/564x/8e/2c/45/8e2c45f0ba71c610699c6696cbed3804.jpg",
-  "28606760851a477da185da5584b6d2f1.mp4",
   "https://i.pinimg.com/564x/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg",
   "https://i.pinimg.com/564x/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg",
   "https://i.pinimg.com/564x/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg",
